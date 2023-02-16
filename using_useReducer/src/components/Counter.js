354fc9f231c6ff1f640917a032dbe2f5ecc949d6
bappyasif <bappyasif@hotmail.com>
2023-02-16 23:44:41 +0600

    proper use case example

--- using_useReducer/src/components/Counter.js
+++ using_useReducer/src/components/Counter.js
@@ -1,25 +1,31 @@
 import React, { useReducer } from 'react'
 
+// this way of updating state is very reliable as this is only place where state is changed and not from any place else, thus makinhg state changes very centralised and informed
 const reducer = (state, action) => {
-    return (
-        {
-            count: state.count + 1
-        }
-    )
+    switch (action.type) {
+        case "increment":
+            return { count: state.count + 1 }
+        case "decrement":
+            return { count: state.count - 1 }
+        default:
+            state
+    }
 }
 
 function Counter() {
     let [state, dispatch] = useReducer(reducer, { count: 0 })
 
-    const increaseCount = () => dispatch()
+    const increaseCount = () => dispatch({type: "increment"})
+
+    const decreaseCount = () => dispatch({type: "decrement"})
 
     return (
         <div>
             <h2>Counter</h2>
-            <section style={{display: "flex"}}>
-                <button style={{padding: "11px", fontSize: "x-large"}}>-</button>
+            <section style={{ display: "flex" }}>
+                <button style={{ padding: "11px", fontSize: "x-large" }} onClick={decreaseCount}>-</button>
                 <h3>{state.count}</h3>
-                <button style={{padding: "11px", fontSize: "x-large"}} onClick={increaseCount}>+</button>
+                <button style={{ padding: "11px", fontSize: "x-large" }} onClick={increaseCount}>+</button>
             </section>
         </div>
     )
