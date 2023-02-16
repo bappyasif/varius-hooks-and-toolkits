c8d721879ba29b2ac4e5a1cf56156ff725640a59
bappyasif <bappyasif@hotmail.com>
2023-02-17 01:02:41 +0600

    another example using useReducer

--- using_useReducer/src/App.js
+++ using_useReducer/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Counter from './components/Counter'
+import Todos from './components/Todos'
 export default class App extends Component {
     render() {
         return(
             <div>
                 <h1>Using Hooks</h1>
                 <Counter />
+                <Todos />
             </div>
         )
     }
