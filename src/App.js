7a263760e86f7c0b8cfc2ae764250e0919e433ef
bappyasif <bappyasif@hotmail.com>
2023-03-01 00:28:39 +0600

    initial commit for this
    * almost like a clean slate for this repo branch

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import React, { Component } from 'react'
-import AnotherExample from './components/AnotherExample'
-import Counter from './components/Counter'
-import Form from './components/exploring_further/Form'
-import Post from './components/exploring_further/Post'
-import Todos from './components/Todos'
-import TodosV2 from './components/Todos-V2'
 export default class App extends Component {
     render() {
         return(
             <div>
-                <h1>Using Hooks</h1>
-                <Counter />
-                <Todos />
-                <TodosV2 />
-                <AnotherExample />
-                <Post />
-                <Form />
+                <h1>Using React Tanstack Query</h1>
             </div>
         )
     }
