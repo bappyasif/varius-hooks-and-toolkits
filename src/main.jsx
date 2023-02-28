399557db6c8c092700189c7377d2feb377e69ef4
bappyasif <bappyasif@hotmail.com>
2023-03-01 01:13:56 +0600

    project is ready with vite and tanstack

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,15 @@
-import { render } from 'preact'
-import { App } from './app'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import App from './App'
 import './index.css'
 
-render(<App />, document.getElementById('app'))
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>,
+)
