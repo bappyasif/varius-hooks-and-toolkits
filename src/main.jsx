4c5932169b1acdca82383f651b206ff733298f3c
bappyasif <bappyasif@hotmail.com>
2023-03-01 21:51:43 +0600

    initial attempt of understanding react query

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import App from './App'
 import './index.css'
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools"
 
 const queryClient = new QueryClient()
 
@@ -10,6 +11,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      {/* so that we can see in what states are our react query is */}
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>,
 )
