f6acb841353d3e6632e2bc13c9a22f6513d896ea
bappyasif <bappyasif@hotmail.com>
2023-03-20 22:24:45 +0600

    used env variables in auth configs

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,4 +1,3 @@
-import { secrets } from "@/secrets";
 import NextAuth from "next-auth/next";
 import GitHubProviders from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
@@ -6,12 +5,12 @@ import GoogleProvider from "next-auth/providers/google"
 export default NextAuth({
     providers: [
         GitHubProviders({
-            clientId: secrets.GITHUB_CLIENT_ID,
-            clientSecret: secrets.GITHUB_CLIENT_SECRET
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET
         }),
         GoogleProvider({
-            clientId: secrets.GOOGLE_CLIENT_ID,
-            clientSecret: secrets.GOOGLE_CLIENT_SECRET
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
     ]
 })
