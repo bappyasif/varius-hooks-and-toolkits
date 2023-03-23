cf348f98f68c0b2da6e76b07b992632039a58e5d
bappyasif <bappyasif@hotmail.com>
2023-03-23 20:06:35 +0600

    rest of styling concepts
    * making use of SASS componets in pages
    * making use of css in js with inlnine styling
    * using styled components with theme in _app.js

--- pages/profile.js
+++ pages/profile.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import styles from "../styles/Profile.module.css"
+import styles2 from "../styles/Profile.module.scss"
+
 const ProfilePage = () => {
   return (
     <>
     <div className='text text-primary'>ProfilePage</div>
     <button className={styles.highlight}>Demo</button>
+    <button className={styles2.highlight}>Demo2</button>
     </>
   )
 }
