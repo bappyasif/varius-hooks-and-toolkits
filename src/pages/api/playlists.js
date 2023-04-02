7abfee1aaaac69bd61b2bfa4248c9fd351058a18
bappyasif <bappyasif@hotmail.com>
2023-04-03 02:04:54 +0600

    strting to work with mongodb client
    * initial setup is done

--- src/pages/api/playlists.js
+++ src/pages/api/playlists.js
@@ -1,4 +1,22 @@
-export default function handler (req, res) {
-    
+import client from "../../lib/mongodb"
+
+// export default function handler (req, res) {
+//     client.then(value => {
+//         // console.log(value.db("user"))
+//         const collection = value.db("users")
+//         const docs = collection?.find({});
+//         console.log(docs)
+//     }).catch(err => console.log(err))
+//     res.status(200).json({msg: "'t is levend"})
+// }
+
+export default async function handler (req, res) {
+    const dbClient = await client;
+    const db = await dbClient.db("nextjsTryout")
+    const collection = db.collection("users")
+    const docs = await collection?.find({name: "a.b."});
+    const data = await docs.data
+    // docs.then(value => console.log(value))
+    // console.log(docs)
     res.status(200).json({msg: "'t is levend"})
 }
