c51386decfefaad246a06f32303c8ab98e761bb2
bappyasif <bappyasif@hotmail.com>
2023-04-06 19:36:11 +0600

    added feature
    * user login with credentials are now in place (even though its not maintained in db sessions as this is one of its constraints and next auth recommends not to use it often)
    * used some custom changes in session to make session data is good enough for our app actions
    * also used some basic validation on user inputs and sees default error message when otherwise

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github"
+import CredentialsProvider from "next-auth/providers/credentials";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
 import clientPromise from "@/lib/mongodb";
 
@@ -13,7 +14,26 @@ export const authOptions = ({
                 clientId: process.env.GITHUB_CLIENT_ID,
                 clientSecret: process.env.GITHUB_CLIENT_SECRET
             }
-        )
+        ),
+        CredentialsProvider({
+            name: "credentials",
+            credentials: {
+                username: {label: "Username", type: "text", placeholder: "john smith", required: true},
+                email: {label: "Email", type: "email", placeholder: "john@smith.com", required: true},
+                password: {label: "Password", type: "password", placeholder: "*********", required: true, minlength: 4}
+            },
+            async authorize(credentials, req) {
+                const {username, email, password} = credentials;
+                // console.log(username, email, password)
+                const user = {name: username, email, password, id: Date.now()}
+
+                if(user && username && email && password) {
+                    return user
+                } else {
+                    return null
+                }
+            }
+        })
     ],
     adapter: MongoDBAdapter(clientPromise),
     session: { strategy: "jwt" },
@@ -26,7 +46,12 @@ export const authOptions = ({
                 token.accessToken = account?.access_token
                 token.id = profile?.id
                 // token.role = profile?.role
+            } else {
+                // this will be used when usaer is logged in using "Email and password"
+                token.accessToken = token?.jti
+                token.id = token.sub
             }
+            // console.log(token, account, profile, "JWT CB!!")
             return token
         },
         async session({ session, token, user }) {
