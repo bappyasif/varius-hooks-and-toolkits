09f6399be328dc14729f456a6a830478c10d3833
bappyasif <bappyasif@hotmail.com>
2023-07-14 01:39:39 +0600

    starting to start building up redux slices

---
+++ src/features/category/categorySlice.ts
@@ -0,0 +1,33 @@
+type CategoryState = {
+    Beef: number,
+    Breakfast: number,
+    Chicken: number,
+    Dessert: number,
+    Goat: number,
+    Lamb: number,
+    Miscellaneous: number,
+    Pasta: number,
+    Pork: number,
+    Seafood: number,
+    Side: number,
+    Starter: number,
+    Vegan: number,
+    Vegetarian: number
+}
+
+const initialStateForCategories: CategoryState = {
+    Beef: 0,
+    Breakfast: 0,
+    Chicken: 0,
+    Dessert: 0,
+    Goat: 0,
+    Lamb: 0,
+    Miscellaneous: 0,
+    Pasta: 0,
+    Pork: 0,
+    Seafood: 0,
+    Side: 0,
+    Starter: 0,
+    Vegan: 0,
+    Vegetarian: 0
+}
