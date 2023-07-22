66cee3f2a8df9bc75ce58389d1eff46e85adbd77
bappyasif <bappyasif@hotmail.com>
2023-07-22 19:12:33 +0600

    meal details now has some basic ui
    * re arranged and adjusted ui to meet visual asthetics
    * used in component rendering method for few items, such as instructions and meal tags
    * users can now also vist cuisine and category from meal details too

--- src/features/meals/MealDetails.tsx
+++ src/features/meals/MealDetails.tsx
@@ -18,9 +18,9 @@ export const MealDetails = () => {
 
     return (
         <div>
-            <h1>MealDetails</h1>
+            {/* <h1>MealDetails</h1> */}
             {/* {measures.length} -- {ingredients.length} */}
-            <RenderMealBasicInfo 
+            <RenderMealBasicInfo
             // ingredients={ingredients} measures={measures} 
             />
         </div>
@@ -34,22 +34,47 @@ type IAMT = {
 
 export const RenderMealBasicInfo = () => {
     const meal = useAppSelector(state => state.meal.meal)
+
     const { category, cuisine, instructions, mealId, mealName, mealSource, mealTags, mealThumb, mealTube } = meal
-    
+
     // const renderIAMT = ()
 
+    const navigate = useNavigate()
+
+    const handleCategoryClick = () => navigate(`/categories/${category}`)
+
+    const handleCuisineClick = () => navigate(`/cuisines/${cuisine}`)
+
+    const renderTags = mealTags?.split(",").map(name => <span key={name}>{name}</span>)
+
+    const renderInstructions = (
+        <div className="flex flex-col items-center">
+            <h2 className="text-4xl">Instructions</h2>
+            <p className="text-2xl w-5/6">{instructions}</p>
+        </div>
+    )
+
     const content = (
-        <div key={mealId}>
+        <div key={mealId} className="flex flex-col items-center gap-y-8">
             <h1>{mealName}</h1>
-            <p>{category}</p>
-            <p>{cuisine}</p>
-            <p>{mealTags}</p>
-            <a href={mealSource}>Source</a>
+
+            <a target="_blank" href={mealSource}>Visit Source Website</a>
+
+            <div className="mx-auto">
+                <img className="aspect-square h-96" src={mealThumb} alt={mealName} />
+
+                <p className="flex gap-4">
+                    <button onClick={handleCategoryClick}>{category}</button>
+                    <button onClick={handleCuisineClick}>{cuisine}</button>
+                </p>
+                <p className="flex gap-4">{renderTags}</p>
+            </div>
+
             <RenderIngredientsAndMeasures />
-            <p>{instructions}</p>
-            <img src={mealThumb} alt={mealName} />
-            {/* <video src={mealTube} controls></video> */}
-            <iframe width="420" height="315"
+
+            {renderInstructions}
+
+            <iframe width="720" height="315"
                 src={`${mealTube.replace("watch?v=", "embed/")}`}>
             </iframe>
         </div>
@@ -61,27 +86,32 @@ export const RenderMealBasicInfo = () => {
 const RenderIngredientsAndMeasures = () => {
     const measures = useAppSelector(state => state.meal.measures);
     const ingredients = useAppSelector(state => state.meal.ingredients)
-    
+
     console.log(measures, ingredients);
 
     const dispatch = useAppDispatch();
 
-    const navigate = useNavigate()
-
     const handleClick = (ingredientName: string) => {
         dispatch(increaseCountForIngredient(ingredientName))
-        navigate(`/ingredients/${ingredientName}`)
+        // navigate(`/ingredients/${ingredientName}`)
     }
 
     const content = (
         ingredients.map((item, idx) => {
             return (
-                <div key={item.text + idx}>
-                    <button onClick={() => handleClick(item.text)}>{item.text}</button> -- <span>{measures[idx].text}</span>
+                <div key={item.text + idx} className="flex gap-4 text-2xl w-96">
+                    <Link to={`/ingredients/${item.text}`} onClick={() => handleClick(item.text)}>{item.text}</Link> -- <span>{measures[idx].text}</span>
                 </div>
             )
         })
     )
 
-    return content
+    return (
+        <div className="flex flex-col items-center gap-y-8">
+            <h2 className="text-4xl">Ingredients and measurements</h2>
+            <div className="flex gap-x-8 gap-y-4 flex-wrap justify-center w-5/6">
+                {content}
+            </div>
+        </div>
+    )
 }
